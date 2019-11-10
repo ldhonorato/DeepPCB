a4e665d9fa04d24b64ea08184bdc6027411622af
Leandro Honorato <ld.honorato@gmail.com>
2019-11-10 12:56:22 -0300

    Adicionando multi-class label

--- src/dataset_prep.py
+++ src/dataset_prep.py
@@ -8,9 +8,9 @@ from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
 def splitTrainingValidationDataset(datasetPath, trainingExamples=1500):
     badFiles = [] #files of pcb with defects
     goodFiles = [] #files of pcb without defects
-
+  
     trainval_txtFile = datasetPath + 'trainval.txt'
-
+    
     #Each line of trainval file has the structure "path/imageName.jpg path/notes.txt"
     #But, there is no imageName.jpg, its just the beginer file name for the templete image and the test image
     #There are two images: imageName_temp.jpg, imageName_test.jpg - template and test image respectively
@@ -20,8 +20,10 @@ def splitTrainingValidationDataset(datasetPath, trainingExamples=1500):
     f = open(trainval_txtFile, "r")
     lines = f.readlines()
     for l in lines:
-        basePath = l.split()[0] #takes only the "path/imageName.jpg"
-        pointIndex = basePath.find('.')
+        paths = l.split()
+        basePath = paths[0] #takes only the "path/imageName.jpg"
+        
+        pointIndex = basePath.find('.') #takes only the "path/anotations.txt"
         badFiles.append(datasetPath + basePath[:pointIndex] + "_test.jpg")
         goodFiles.append(datasetPath + basePath[:pointIndex] + "_temp.jpg")
     f.close()
@@ -66,6 +68,31 @@ def getTestImageFiles(datasetPath='DeepPCB/PCBData/', test_dir='PCB_test_data'):
 
     return test_files
 
+def getAnotationPath(testFilePath):
+    splited_path = testFilePath.split('/')
+    anotations_path = ''
+    for i in range(0, len(splited_path)-2):
+        anotations_path += splited_path[i] + '/'
+    
+    anotations_fileName = splited_path[-1]
+    removeIndex = anotations_fileName.find('_test')
+    anotations_fileName = anotations_fileName[0:removeIndex] + '.txt'
+    anotations_path += splited_path[-2] + '_not/' + anotations_fileName
+    
+    return anotations_path
+
+def getLabelsFromAnotation(anotationFilePath):
+    file = open(anotationFilePath, "r")
+    contents = file.readlines()
+    labels = [0,0,0,0,0,0]
+    for line in contents:
+        line = line.strip("\n")
+        idx = int(line[-1])
+        labels[idx-1] = 1
+    file.close()
+    
+    return labels
+
 def saveFilesToPath(files_list, dest_path):
     if os.path.exists(dest_path) and os.path.isdir(dest_path):
        shutil.rmtree(dest_path)
@@ -73,14 +100,27 @@ def saveFilesToPath(files_list, dest_path):
     os.mkdir(dest_path)
 
     for fn in files_list:
-        shutil.copy(fn, dest_path)
+        labels = [0,0,0,0,0,0]
+        if 'test' in fn:
+            anotations_path = getAnotationPath(fn)
+            labels = getLabelsFromAnotation(anotations_path)
+        
+        imageName = fn.split('/')[-1]
+        imageName = imageName[0:-4] + ''.join(map(str, labels)) + imageName[-4:]
+        shutil.copy(fn, dest_path + '/' + imageName)
 
-def loadImagesFromFolder(folderPath, imgDim=(224,224)):
+def loadImagesFromFolder(folderPath, imgDim=(224,224), multi_label=False):
     files = glob.glob(folderPath + '/*')
     #images = [img_to_array(load_img(img, target_size=imgDim))[:,:,0:1] for img in files]
     images = [img_to_array(load_img(img, target_size=imgDim)) for img in files]
     images = np.array(images)
-    labels = ["test" in fn for fn in files]
+    
+    if multi_label:
+        labels = []
+        for fn in files:
+            labels.append(list(map(int, list(fn[-10:-4]))))
+    else:
+        labels = [1 if "test" in fn.split('/')[-1] else 0 for fn in files]
     return (images, labels)
 
 def number2label(numberClass):
@@ -89,18 +129,7 @@ def number2label(numberClass):
 def class2number(labelClass):
     return np.array(labelClass)*1 #labelClass must be boolean array
 
-def loadTestImagesFromFolder(test_dir='PCB_test_data', imgDim=(224,224)):
-    test_files = glob.glob(test_dir + '/*')
-    test_imgs = [img_to_array(load_img(img, target_size=imgDim)) for img in test_files]
-    test_imgs = np.array(test_imgs)
-    test_labels = ["test" in fn.split('/')[1] for fn in test_files]
-    test_imgs_scaled = test_imgs / 255.
-    test_labels_enc = class2number(test_labels)
-
-    print('Test dataset shape:', test_imgs.shape)
-    return (test_imgs, test_imgs_scaled, test_labels, test_labels_enc)
-
-def run_dataset_preparation(datasetPath='DeepPCB/PCBData/',train_dir='PCB_training_data', val_dir = 'PCB_validation_data', IMG_DIM=(224,224)):
+def run_dataset_preparation(datasetPath='DeepPCB/PCBData/',train_dir='PCB_training_data', val_dir = 'PCB_validation_data', IMG_DIM=(224,224), multi_label = False):
     shutil.rmtree(train_dir, ignore_errors=True)
     shutil.rmtree(val_dir, ignore_errors=True)
 
@@ -109,22 +138,25 @@ def run_dataset_preparation(datasetPath='DeepPCB/PCBData/',train_dir='PCB_traini
     saveFilesToPath(training_files, train_dir)
     saveFilesToPath(validation_files, val_dir)
 
-    (train_imgs, train_labels) = loadImagesFromFolder(train_dir, IMG_DIM)
-    (validation_imgs, validation_labels) = loadImagesFromFolder(val_dir, IMG_DIM)
+    (train_imgs, train_labels) = loadImagesFromFolder(train_dir, IMG_DIM, multi_label)
+    (validation_imgs, validation_labels) = loadImagesFromFolder(val_dir, IMG_DIM, multi_label)
 
-    train_imgs_scaled = train_imgs / 255.
-    validation_imgs_scaled = validation_imgs / 255.
+#    train_imgs_scaled = train_imgs / 255.
+#    validation_imgs_scaled = validation_imgs / 255.
 
     #le = LabelEncoder()
     #le.fit(train_labels)
     #train_labels_enc = le.transform(train_labels)
     #validation_labels_enc = le.transform(validation_labels)
-    train_labels_enc = class2number(train_labels)
-    validation_labels_enc = class2number(validation_labels)
+    
+#    if not multi_label:
+#        train_labels_enc = class2number(train_labels)
+#        validation_labels_enc = class2number(validation_labels)
 
     print('Trainining files = ', len(training_files))
     print('Validation files = ', len(validation_files))
-    return (train_imgs, train_imgs_scaled, train_labels, train_labels_enc), (validation_imgs, validation_imgs_scaled, validation_labels, validation_labels_enc)
+    #return (train_imgs, train_imgs_scaled, train_labels, train_labels_enc), (validation_imgs, validation_imgs_scaled, validation_labels, validation_labels_enc)
+    return (train_imgs, train_labels), (validation_imgs, validation_labels)
 
 
 
